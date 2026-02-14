52be2e994eba6d4efa5597c43a989a67e84f0c4a
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2026-02-13 21:26:16 -0300

    resistor-color-duo

---
+++ lua/resistor-color-duo/resistor-color-duo.lua
@@ -0,0 +1,18 @@
+return {
+	value = function(colors)
+		resistor_value = {
+			black = 0,
+			brown = 1,
+			red = 2,
+			orange = 3,
+			yellow = 4,
+			green = 5,
+			blue = 6,
+			violet = 7,
+			grey = 8,
+			white = 9,
+		}
+
+		return tonumber(resistor_value[colors[1]] .. resistor_value[colors[2]])
+	end,
+}
