8d3135e11cf635d0b357c6f303a9303f0950bcdb
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2026-02-17 19:59:28 -0300

    raindrops

--- lua/hello-world/run_tests.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set LUA_PATH=C:\Users\Gustavo\AppData\Roaming\luarocks\share\lua\5.4\?.lua;C:\Users\Gustavo\AppData\Roaming\luarocks\share\lua\5.4\?\init.lua;;
-set LUA_CPATH=C:\Users\Gustavo\AppData\Roaming\luarocks\lib\lua\5.4\?.dll;;
-lua C:\Users\Gustavo\AppData\Roaming\luarocks\bin\busted
--- lua/raindrops/raindrops.lua
+++ lua/raindrops/raindrops.lua
@@ -1,2 +1,16 @@
 return function(n)
+	local response = ""
+	if n % 3 == 0 then
+		response = response .. "Pling"
+	end
+	if n % 5 == 0 then
+		response = response .. "Plang"
+	end
+	if n % 7 == 0 then
+		response = response .. "Plong"
+	end
+	if response == "" then
+		return tostring(n)
+	end
+	return response
 end
