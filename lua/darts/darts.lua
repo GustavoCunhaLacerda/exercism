2ff34376d28a18bafde539b8e16cce01966cbd01
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2026-02-17 19:59:15 -0300

    darts

--- lua/darts/darts.lua
+++ lua/darts/darts.lua
@@ -1,6 +1,17 @@
 local Darts = {}
 
 function Darts.score(x, y)
+	local hipotenusa = ((x ^ 2) + (y ^ 2)) ^ 0.5
+
+	if hipotenusa <= 1 then
+		return 10
+	elseif hipotenusa <= 5 then
+		return 5
+	elseif hipotenusa <= 10 then
+		return 1
+	end
+
+	return 0
 end
 
 return Darts
